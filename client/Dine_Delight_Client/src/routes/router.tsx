068c4a19bc320b9f3bcf033ddeda85459c7d183e
Muhammed Parvesh M <parveshrio001@gmail.com>
2024-03-26 21:05:05 +0530

    Lazy loading and suspense is added to the routing

--- client/Dine_Delight_Client/src/routes/router.tsx
+++ client/Dine_Delight_Client/src/routes/router.tsx
@@ -1,159 +1,181 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import AccountPage from "../pages/user/AccountPage";
-import Login from "../pages/user/Login";
-import SignUp from "../pages/user/signup";
-import RestaurantLogin from "../pages/restaurant/restaurantLogin";
-import VerifyOTP from "../pages/user/VerifyOTP";
-import RestaurantSignup from "../pages/restaurant/restaurantSignup";
-import SellerPage from "../pages/restaurant/SellerPage";
-import EmailVerificationPage from "../pages/restaurant/emailVerification";
-import AdminLogin from "../pages/admin/AdminLogin";
+
 import ProtectedRoute, {
   AdminProtectedRoute,
   SellerProtectedRoute,
 } from "./ProtectedRoutes";
 import PublicRoute from "./PublicRoutes";
-import ForgotPassword from "../pages/user/forgotPassword";
-import ResetPassword from "../pages/user/resetPassword";
-import AdminPage from "../pages/admin/AdminPage";
-import UsersList from "../pages/admin/UserList";
-import RestaurantList from "../pages/admin/RestaurantList";
-import NewRegistration from "../pages/admin/newRegistration";
-// import AdminDashboard from "../pages/admin/AdminDashboard";
-import NotFoundPage from "../pages/Error404";
-import ViewRestaurant from "../pages/restaurant/restaurantDetails";
-// import Home from "../pages/Home";
-import Tables from "../pages/restaurant/Tables/Tables";
-import TimeSlots from "../pages/restaurant/Tables/timeSlots";
-import ViewTable from "../pages/restaurant/Tables/viewTableSlots";
-import RestaurantView from "../pages/user/RestaurantViewPage";
-import BookTable from "../pages/user/BookTable";
-import PaymentCompleted from "../pages/user/payment/PaymentCompleted";
-import BookingHistory from "../pages/user/BookingHistory";
-import Reservations from "../pages/restaurant/Reservations";
-import ViewReservation from "../pages/restaurant/viewReservation";
-import ViewBooking from "../pages/user/viewBooking";
-import Profile from "../pages/user/Profile";
-import TransactionHistory from "../pages/user/TransactionHistory";
-import { Suspense, lazy } from "react";
+
+const AccountPage = lazy(() => import("../pages/user/AccountPage"));
+const Login = lazy(() => import("../pages/user/Login"));
+const SignUp = lazy(() => import("../pages/user/signup"));
+const RestaurantLogin = lazy(
+  () => import("../pages/restaurant/restaurantLogin")
+);
+const VerifyOTP = lazy(() => import("../pages/user/VerifyOTP"));
+const RestaurantSignup = lazy(
+  () => import("../pages/restaurant/restaurantSignup")
+);
+const SellerPage = lazy(() => import("../pages/restaurant/SellerPage"));
+const EmailVerificationPage = lazy(
+  () => import("../pages/restaurant/emailVerification")
+);
+const AdminLogin = lazy(() => import("../pages/admin/AdminLogin"));
+const ForgotPassword = lazy(() => import("../pages/user/forgotPassword"));
+const ResetPassword = lazy(() => import("../pages/user/resetPassword"));
+const AdminPage = lazy(() => import("../pages/admin/AdminPage"));
+const UsersList = lazy(() => import("../pages/admin/UserList"));
+const RestaurantList = lazy(() => import("../pages/admin/RestaurantList"));
+const NewRegistration = lazy(() => import("../pages/admin/newRegistration"));
+const NotFoundPage = lazy(() => import("../pages/Error404"));
+const ViewRestaurant = lazy(
+  () => import("../pages/restaurant/restaurantDetails")
+);
+const Tables = lazy(() => import("../pages/restaurant/Tables/Tables"));
+const TimeSlots = lazy(() => import("../pages/restaurant/Tables/timeSlots"));
+const ViewTable = lazy(
+  () => import("../pages/restaurant/Tables/viewTableSlots")
+);
+const RestaurantView = lazy(() => import("../pages/user/RestaurantViewPage"));
+const BookTable = lazy(() => import("../pages/user/BookTable"));
+const PaymentCompleted = lazy(
+  () => import("../pages/user/payment/PaymentCompleted")
+);
+const BookingHistory = lazy(() => import("../pages/user/BookingHistory"));
+const Reservations = lazy(() => import("../pages/restaurant/Reservations"));
+const ViewReservation = lazy(
+  () => import("../pages/restaurant/viewReservation")
+);
+const ViewBooking = lazy(() => import("../pages/user/viewBooking"));
+const Profile = lazy(() => import("../pages/user/Profile"));
+const TransactionHistory = lazy(
+  () => import("../pages/user/TransactionHistory")
+);
+
 const Home = lazy(() => import("../pages/Home"));
 const AdminDashboard = lazy(() => import("../pages/admin/AdminDashboard"));
 
 export const MainRouter = () => {
   return (
-    <Suspense fallback={<h1>Loading</h1>}>
-      <Routes>
-        {/******************* User routes *****************/}
-        <Route path="/" element={<Home />} />
-        <Route path="/view_restaurant/:id" element={<RestaurantView />} />
-        <Route path="" element={<PublicRoute />}>
-          <Route path="/user/auth/signup" element={<SignUp />} />
-          <Route path="/user/auth/verify_otp" element={<VerifyOTP />} />
-          <Route path="/user/auth/login" element={<Login />} />
-          <Route
-            path="/user/auth/forgot_password"
-            element={<ForgotPassword />}
-          />
-          <Route
-            path="/user/auth/reset_password/:id"
-            element={<ResetPassword />}
-          />
-        </Route>
-        {/* </Route>  */}
+    <>
+      <Suspense fallback={<h1>Loading</h1>}>
+        <Routes>
+          {/******************* User routes *****************/}
+          <Route path="/" element={<Home />} />
 
-        {/* User Protected Route  */}
-        <Route path="" element={<ProtectedRoute />}>
-          <Route
-            path="/profile"
-            element={<AccountPage children={<Profile />} />}
-          />
-          <Route path="/reserve_table" element={<BookTable />} />
-          <Route path="/payment_status/:id" element={<PaymentCompleted />} />
-          <Route
-            path="/booking_history"
-            element={<AccountPage children={<BookingHistory />} />}
-          />
-          <Route
-            path="booking/view/:id"
-            element={<AccountPage children={<ViewBooking />} />}
-          />
-          <Route
-            path="/transaction_history"
-            element={<AccountPage children={<TransactionHistory />} />}
-          />
-        </Route>
+          <Route path="/view_restaurant/:id" element={<RestaurantView />} />
+          <Route path="" element={<PublicRoute />}>
+            <Route path="/user/auth/signup" element={<SignUp />} />
+            <Route path="/user/auth/verify_otp" element={<VerifyOTP />} />
+            <Route path="/user/auth/login" element={<Login />} />
+            <Route
+              path="/user/auth/forgot_password"
+              element={<ForgotPassword />}
+            />
+            <Route
+              path="/user/auth/reset_password/:id"
+              element={<ResetPassword />}
+            />
+          </Route>
 
-        {/******************* Restaurant routes *****************/}
-        <Route path="restaurant/auth/" element={<PublicRoute />}>
-          <Route path="login" element={<RestaurantLogin />} />
-          <Route path="signup" element={<RestaurantSignup />} />
-          <Route
-            path="verify_token/:token"
-            element={<EmailVerificationPage />}
-          />
-        </Route>
-        {/* Restaurant private routes  */}
-        <Route path="/restaurant/" element={<SellerProtectedRoute />}>
-          <Route index element={<Navigate to="dashboard" />} />
-          <Route
-            path="dashboard"
-            element={<SellerPage children={<div>Dashboard</div>} />}
-          />
-          <Route
-            path="reservations"
-            element={<SellerPage children={<Reservations />} />}
-          />
-          <Route
-            path="reservations/view/:id"
-            element={<SellerPage children={<ViewReservation />} />}
-          />
-          <Route path="table" element={<SellerPage children={<Tables />} />} />
-          <Route
-            path="view_table/:id"
-            element={<SellerPage children={<ViewTable />} />}
-          />
-          <Route
-            path="time_slots"
-            element={<SellerPage children={<TimeSlots />} />}
-          />
-          <Route
-            path="menu"
-            element={<SellerPage children={<div>Food menu</div>} />}
-          />
-          <Route
-            path="view"
-            element={<SellerPage children={<ViewRestaurant />} />}
-          />
-        </Route>
+          {/* User Protected Route  */}
+          <Route path="" element={<ProtectedRoute />}>
+            <Route
+              path="/profile"
+              element={<AccountPage children={<Profile />} />}
+            />
+            <Route path="/reserve_table" element={<BookTable />} />
+            <Route path="/payment_status/:id" element={<PaymentCompleted />} />
+            <Route
+              path="/booking_history"
+              element={<AccountPage children={<BookingHistory />} />}
+            />
+            <Route
+              path="booking/view/:id"
+              element={<AccountPage children={<ViewBooking />} />}
+            />
+            <Route
+              path="/transaction_history"
+              element={<AccountPage children={<TransactionHistory />} />}
+            />
+          </Route>
 
-        {/******************* Admin routes *****************/}
-        <Route path="/admin/auth/" element={<PublicRoute />}>
-          <Route path="login" element={<AdminLogin />} />
-        </Route>
+          {/******************* Restaurant routes *****************/}
+          <Route path="restaurant/auth/" element={<PublicRoute />}>
+            <Route path="login" element={<RestaurantLogin />} />
+            <Route path="signup" element={<RestaurantSignup />} />
+            <Route
+              path="verify_token/:token"
+              element={<EmailVerificationPage />}
+            />
+          </Route>
+          {/* Restaurant private routes  */}
+          <Route path="/restaurant/" element={<SellerProtectedRoute />}>
+            <Route index element={<Navigate to="dashboard" />} />
+            <Route
+              path="dashboard"
+              element={<SellerPage children={<div>Dashboard</div>} />}
+            />
+            <Route
+              path="reservations"
+              element={<SellerPage children={<Reservations />} />}
+            />
+            <Route
+              path="reservations/view/:id"
+              element={<SellerPage children={<ViewReservation />} />}
+            />
+            <Route
+              path="table"
+              element={<SellerPage children={<Tables />} />}
+            />
+            <Route
+              path="view_table/:id"
+              element={<SellerPage children={<ViewTable />} />}
+            />
+            <Route
+              path="time_slots"
+              element={<SellerPage children={<TimeSlots />} />}
+            />
+            <Route
+              path="menu"
+              element={<SellerPage children={<div>Food menu</div>} />}
+            />
+            <Route
+              path="view"
+              element={<SellerPage children={<ViewRestaurant />} />}
+            />
+          </Route>
 
-        {/* admin protected Route  */}
-        <Route path="/admin" element={<AdminProtectedRoute />}>
-          <Route
-            path="dashboard"
-            element={<AdminPage children={<AdminDashboard />} />}
-          />
-          <Route
-            path="users"
-            element={<AdminPage children={<UsersList />} />}
-          />
-          <Route
-            path="restaurants"
-            element={<AdminPage children={<RestaurantList />} />} //passing  component as props
-          />
-          <Route
-            path="new_registrations"
-            element={<AdminPage children={<NewRegistration />} />}
-          />
-        </Route>
-        <Route path="banners" element={<div>Banners</div>} />
+          {/******************* Admin routes *****************/}
+          <Route path="/admin/auth/" element={<PublicRoute />}>
+            <Route path="login" element={<AdminLogin />} />
+          </Route>
 
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Suspense>
+          {/* admin protected Route  */}
+          <Route path="/admin" element={<AdminProtectedRoute />}>
+            <Route
+              path="dashboard"
+              element={<AdminPage children={<AdminDashboard />} />}
+            />
+            <Route
+              path="users"
+              element={<AdminPage children={<UsersList />} />}
+            />
+            <Route
+              path="restaurants"
+              element={<AdminPage children={<RestaurantList />} />} //passing  component as props
+            />
+            <Route
+              path="new_registrations"
+              element={<AdminPage children={<NewRegistration />} />}
+            />
+          </Route>
+          <Route path="banners" element={<div>Banners</div>} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 };
